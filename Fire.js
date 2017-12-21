58d417c446462e73720994a85994718e20d02bbc
Tobias <tobias.ohlsson123@gmail.com>
2017-12-21 19:31:18 +0100

    test

--- Fire.js
+++ Fire.js
@@ -83,7 +83,6 @@ function display() {
 
 
     if(meter != null) {
-        startText.innerHTML = meter.volume;
         if(meter.volume > 0.35) {
             smaller();
         }
@@ -137,7 +136,7 @@ window.onclick = function (ev) { var x_Mouse = event.clientX;     // Get the hor
 
 window.ondevicemotion = function (ev) {
     var maxAcceleration = Math.max(Math.abs(ev.acceleration.x), Math.abs(ev.acceleration.y), Math.abs(ev.acceleration.z));
-    if(maxAcceleration > 10){
+    startText.innerHTML = maxAcceleration;
        fires = [];
        count = 80;
        sizeMult = 8;
