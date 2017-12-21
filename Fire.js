9acd54d47c99ee98bffa7ee1053ca478933741d0
Tobias <tobias.ohlsson123@gmail.com>
2017-12-21 17:45:37 +0100

    test

--- Fire.js
+++ Fire.js
@@ -91,7 +91,7 @@ function display() {
 
     if(meter != null) {
         startText.innerHTML = meter.volume;
-        if(meter.volume > 0.75) {
+        if(meter.volume > 0.4) {
             smaller();
         }
     }
